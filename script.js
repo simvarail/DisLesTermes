5c16ab3e1c537473fe549020844e0a025961069e
simvarail <simvarail@gmail.com>
2021-10-08 11:59:17 +0200

    Responsive

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-$(document).ready(function() {
-    var pixelToMove = $(window).width()/30;
-    $("#background-image").mousemove(function(e) {
-      var width = $(this).innerWidth();
-      var height = $(this).innerHeight();
-      var newValueX = (e.pageX / width) * pixelToMove;
-      var newValueY = (e.pageY / height) * pixelToMove;
-      $(this).css('background-position', newValueX + '%' + ' ' + newValueY + '%');
-    });
+  $(function() {
+
+    if ($(window).width()>480){ 
+      $(document).ready(function() {
+        var pixelToMove = $(window).width()/27;
+        $("#background-image").mousemove(function(e) {
+          var width = $(this).innerWidth();
+          var height = $(this).innerHeight();
+          var newValueX = (e.pageX / width) * pixelToMove;
+          var newValueY = (e.pageY / height) * pixelToMove;
+          $(this).css('background-position', newValueX + '%' + ' ' + newValueY + '%');
+        });
+      });
+     }
+  
+   else{
+
+   }
+  
   });
